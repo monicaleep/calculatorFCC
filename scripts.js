cf44d86117f957aaab8771370ad902e29a4e7ce0
monicaparrillo <monica.parrillo@gmail.com>
2017-07-13 20:22:51 -0400

    work on adding

--- scripts.js
+++ scripts.js
@@ -1,13 +1,51 @@
 const display = document.querySelector(".display");
-var arr = [];
+var numberArr = [];
+var masterArr = [];
+const operators = ["+","-","*","/"];
+var answer;
 const numberButtons = document.querySelectorAll(".math-number");
 const equalButton = document.querySelector(".math-equals")
+const addButton = document.querySelector("#plus");
+// Add event listeners to the numbers
+
 for (let i =0; i<numberButtons.length; i++){
   numberButtons[i].addEventListener("click",function(){
-    arr.push(parseInt(this.textContent));
-    display.textContent = parseInt(arr.join(""));
+    //if we just performed a calculation, and want to perform a separate one
+    if (masterArr.length === 1){
+      masterArr = [];
+      numberArr.push(parseInt(this.textContent));
+      display.textContent = numberArr.join("");
+    }
+    //Store the value of each number in an array.
+    else{
+      numberArr.push(parseInt(this.textContent));
+      display.textContent = Array(masterArr+ numberArr).join("").replace(/,/g,"");
+    }
   })
 }
+
+addButton.addEventListener("click",function(){
+  //check either array isn't empty
+  if (numberArr.length > 0 || masterArr.length>0){
+      //store current number in master array,
+      if (numberArr.length > 0){
+        masterArr.push(parseInt(numberArr.join("")));
+      }
+      //add + to master array
+      masterArr.push("+");
+      display.textContent = masterArr.join("");
+      numberArr = [];
+  }
+})
+
 equalButton.addEventListener("click",function(){
-  display.textContent = parseInt(arr.join(""));
+  if (numberArr.length > 0){
+    masterArr.push(parseInt(numberArr.join("")));
+    answer = eval(masterArr.join(""));
+    display.textContent = answer;
+    numberArr = [];
+    masterArr = [];
+    masterArr[0] = answer;
+  }
+
 })
