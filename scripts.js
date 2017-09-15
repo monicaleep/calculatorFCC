c10cf445da766727e27e30ca53e6aed1e3d5af80
monicaleep <monica.parrillo@gmail.com>
2017-09-15 11:02:23 -0400

    add decimal functionality, remove shuffle BS

--- scripts.js
+++ scripts.js
@@ -1,99 +1,101 @@
 //Todos:
 // add decimal
 // refactor code to object-oriented, all methods on a calculator
-// use objects to store all operands
+
 
 const display = document.querySelector(".display");
-var buffer = [];
-var stack = [];
-const operators = ["+","-","*","/"];
+let buffer = [];
+let stack = [];
+const operators = ["+", "-", "*", "/"];
 const operatorButtons = document.querySelectorAll(".math-operator");
-var answer;
 const numberButtons = document.querySelectorAll(".math-number");
 const equalButton = document.querySelector(".math-equals")
 const clearButton = document.querySelector("#clear");
+const decimal = document.querySelector("#decimal");
 
-// Add event listeners to the numbers
-for (let i =0; i<numberButtons.length; i++){
-  numberButtons[i].addEventListener("click",addNumber)
-}
-
-//randomly add the buttons to calculator
-(function (){
-  //not DRY
-  const all = document.querySelectorAll('.math');
-  const newAll = shuffle(Array.from(all));
-  const calc = document.querySelector('.calculator');
-  for (let m =0; m<newAll.length; m++){
-    calc.appendChild(newAll[m]);
-  }
-})();
+// Add event listeners
+numberButtons.forEach(button =>button.addEventListener("click", handleNumber))
+decimal.addEventListener('click', handleDecimal)
+clearButton.addEventListener("click", handleClear)
+operatorButtons.forEach(button => button.addEventListener("click", handleOperator))
+equalButton.addEventListener("click", handleEquals)
 
 
-
-//given an array, randomly shuffle its elements
-//adapted from fisher-yates algorithm on wikipedia
-function shuffle(arr){
-  arr1 = arr;
-  let n = arr.length
-  for (let i =0; i<n-1; i++){
-    let j = Math.floor(Math.random() * (n));
-    let hold = arr[i];
-    arr1[i] = arr1[j];
-    arr[j] = hold;
-    //console.log(arr1)
-  }
-  return arr;
-}
-
-
-//not DRY
-function addNumber(){
-  if (stack.length === 1){
-    stack = [];
-    buffer.push(parseInt(this.textContent));
-    display.textContent = buffer.join("");
-  }
-  //Store the value of each number in an array.
-  else{
-    buffer.push(parseInt(this.textContent));
-    display.textContent = Array(stack+ buffer).join("").replace(/,/g,"");
-  }
-}
-
-//add operator button event listeners. could combine with number event listeners
-for (var i=0;i<operatorButtons.length; i++){
-  operatorButtons[i].addEventListener("click",function(){
-    //check either array isn't empty
-    if (buffer.length > 0){
-        //store current number in master array,
-        stack.push(parseInt(buffer.join("")));
-        //add + to master array
-        stack.push(this.textContent);
-        display.textContent = stack.join("");
-        buffer = [];
-    }
-    else if (stack.length >0 && operators.indexOf(stack[stack.length-1]) == -1){
-      stack.push(this.textContent);
-      display.textContent = stack.join("");
-    }
-  })
-}
-
-
-equalButton.addEventListener("click",function(){
-  if (buffer.length > 0){
-    stack.push(parseInt(buffer.join("")));
-    answer = eval(stack.join(""));
-    display.textContent = answer;
+function handleEquals(){
+  if (buffer.length > 0) {
+    stack.push(parseFloat(buffer.join("")));
+    const answer = eval(stack.join(""));
+    display.textContent = Math.round((answer * 10000000000)) / 10000000000;
     buffer = [];
     stack = [];
     stack[0] = answer;
   }
-})
+}
 
-clearButton.addEventListener("click",function(){
+function handleOperator(){
+  //check buffer array isn't empty
+  if (buffer.length > 0) {
+    //store current number in stack ,
+    stack.push(parseFloat(buffer.join("")));
+    //add operator to stack
+    stack.push(this.textContent);
+    display.textContent = stack.join("");
+    buffer = [];
+  } else if (stack.length > 0 && operators.indexOf(stack[stack.length - 1]) == -1) {
+    stack.push(this.textContent);
+    display.textContent = stack.join("");
+  }
+}
+
+function handleClear(){
   stack = [];
   buffer = [];
   display.textContent = "0";
-})
+}
+
+function handleDecimal(){
+  if (buffer.length === 0){
+    buffer.push(0, ".");
+    display.textContent = stack.concat(buffer).join("").replace(/,/g, "")
+  }
+  else if( buffer.indexOf(".") == -1){
+    buffer.push(".")
+    display.textContent = stack.concat(buffer).join("").replace(/,/g, "")
+  }
+}
+
+function handleNumber() {
+  buffer.push(parseInt(this.textContent));
+  if (stack.length === 1) {
+    stack = [];
+    display.textContent = buffer.join("");
+  }
+  else {
+    display.textContent = stack.concat(buffer).join("").replace(/,/g, "");
+  }
+}
+
+
+//randomly add the buttons to calculator
+// (function (){
+//   //not DRY
+//   const all = document.querySelectorAll('.math');
+//   const newAll = shuffle(Array.from(all));
+//   const calc = document.querySelector('.calculator');
+//   for (let m =0; m<newAll.length; m++){
+//     calc.appendChild(newAll[m]);
+//   }
+// })();
+//given an array, randomly shuffle its elements
+//adapted from fisher-yates algorithm on wikipedia
+// function shuffle(arr){
+//   arr1 = arr;
+//   let n = arr.length
+//   for (let i =0; i<n-1; i++){
+//     let j = Math.floor(Math.random() * (n));
+//     let hold = arr[i];
+//     arr1[i] = arr1[j];
+//     arr[j] = hold;
+//   }
+//   return arr;
+// }
